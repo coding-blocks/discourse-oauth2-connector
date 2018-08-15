f4d308e8a0ee0bea65e0e8a94655acc105d303ae
Archit Kaushik <asarchit65@gmail.com>
2018-08-15 06:17:18 +0530

    made changes

--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,10 @@ const MemoryStore = memorystore(session)
 
 const app: Application = express()
 
-const hbs = exphbs.create({defaultLayout: 'main'});
+const hbs = exphbs.create({defaultLayout: 'main',extname: '.hbs'});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
